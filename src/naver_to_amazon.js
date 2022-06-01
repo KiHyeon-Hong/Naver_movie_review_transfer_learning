be2bd78d738e0ade4efe5e3e168c3c4f2d1077d0
Ki Hyeon Hong <70433404+KiHyeon-Hong@users.noreply.github.com>
2022-06-01 12:51:38 +0900

    update code

--- src/naver_to_amazon.js
+++ src/naver_to_amazon.js
@@ -9,6 +9,8 @@ const datas = fs
   });
 
 fs.writeFileSync(__dirname + '/../files/amazonTemplate.txt', '', 'utf8');
+fs.writeFileSync(__dirname + '/../files/amazonTemplate_pos.txt', '', 'utf8');
+fs.writeFileSync(__dirname + '/../files/amazonTemplate_neg.txt', '', 'utf8');
 
 for (let i = 0; i < datas.length; i++) {
   let temp = '';
@@ -24,4 +26,10 @@ for (let i = 0; i < datas.length; i++) {
   while (temp.length < 100) temp += ' ' + datas[i][1];
 
   fs.appendFileSync(__dirname + '/../files/amazonTemplate.txt', temp + '\n', 'utf8');
+
+  if (datas[i][0] < 7) {
+    fs.appendFileSync(__dirname + '/../files/amazonTemplate_neg.txt', temp + '\n', 'utf8');
+  } else {
+    fs.appendFileSync(__dirname + '/../files/amazonTemplate_pos.txt', temp + '\n', 'utf8');
+  }
 }
--- src/naver_to_imdb.js
+++ src/naver_to_imdb.js
@@ -8,8 +8,10 @@ const datas = fs
     return [parseInt(v[0]), v[1]];
   });
 
-let neg = 0;
-let pos = 0;
+let neg_train = 0;
+let pos_train = 0;
+let neg_test = 0;
+let pos_test = 0;
 
 for (let i = 0; i < datas.length; i++) {
   let temp = datas[i][1];
@@ -17,8 +19,16 @@ for (let i = 0; i < datas.length; i++) {
   while (temp.length < 1000) temp += ' ' + datas[i][1];
 
   if (datas[i][0] < 7) {
-    fs.writeFileSync(__dirname + `/../files/neg/${neg++}_${datas[i][0]}.txt`, temp, 'utf8');
+    if (neg_test < 10000) {
+      fs.writeFileSync(__dirname + `/../files/neg_test/${neg_test++}_${datas[i][0]}.txt`, temp, 'utf8');
+    } else {
+      fs.writeFileSync(__dirname + `/../files/neg_train/${neg_train++}_${datas[i][0]}.txt`, temp, 'utf8');
+    }
   } else {
-    fs.writeFileSync(__dirname + `/../files/pos/${pos++}_${datas[i][0]}.txt`, temp, 'utf8');
+    if (pos_test < 10000) {
+      fs.writeFileSync(__dirname + `/../files/pos_test/${pos_test++}_${datas[i][0]}.txt`, temp, 'utf8');
+    } else {
+      fs.writeFileSync(__dirname + `/../files/pos_train/${pos_train++}_${datas[i][0]}.txt`, temp, 'utf8');
+    }
   }
 }
